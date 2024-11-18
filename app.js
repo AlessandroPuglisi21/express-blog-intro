b086629c055849b698e96ba42ea636adf27c7bbe
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-11-18 15:18:53 +0100

    ADD: Creazione Bacheca

--- app.js
+++ app.js
@@ -13,14 +13,41 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get("/", (req,res) => {
     res.send("SERVER DEL MIO BLOG")
 })
 
 app.get("/bacheca", (req,res) => {
-    res.json({
-        
-    })
+     const products = [
+        {titolo: "Ciambellone alla crema",
+            contenuto : "Goloso ciambellone farcito di crema fatta in casa",
+            immagine : "public/images/ciambellone.jpeg",
+            tags : ["pasticceria","crema","goloso"]
+        },
+        {titolo: "Cracker alla Barbabietola",
+            contenuto : "6 Pacchetti di crackers alla farina di barbabietola",
+            immagine : "public/images/cracker_barbabietola.jpeg",
+            tags : ["healty","panetteria","barbabietola"]
+        },
+        {titolo: "Pane dolce fritto",
+            contenuto : "Pane fresco fritto in olio di girasole e zuccherato",
+            immagine : "public/images/pane_fritto_dolce.jpeg",
+            tags : ["pane","panetteria","pasticceria"]
+        },
+        {titolo : "Pasta alla barbabietola",
+            contenuto : "Pasta trafilata al bronzo con farina di barbabietola",
+            immagine : "public/images/pasta_barbabietola.jpeg",
+            tags : ["healty", "pasta","barbabietola"]
+        },
+        {titolo : "Torta paesana",
+            contenuto : "Tipica torta fatta in casa",
+            immagine : "public/images/torta_paesana.jpeg",
+            tags : ["pasticceria", "torta", "goloso"]
+        }
+    ]
+        res.json(products)
 })
 
 
