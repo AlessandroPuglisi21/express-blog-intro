057c480389479abca34d9f2085d6d8412eabcba6
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-11-18 15:46:19 +0100

    ADDED: Module in another js File

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ console.log('test')
 const express = require('express');
 const app = express();
 const port = 3000;
+const prodotti = require('./prodotti.js')
 
 app.use(express.static('public'));
 
@@ -20,34 +21,37 @@ app.get("/", (req,res) => {
 })
 
 app.get("/bacheca", (req,res) => {
-     const products = [
-        {titolo: "Ciambellone alla crema",
-            contenuto : "Goloso ciambellone farcito di crema fatta in casa",
-            immagine : "public/images/ciambellone.jpeg",
-            tags : ["pasticceria","crema","goloso"]
-        },
-        {titolo: "Cracker alla Barbabietola",
-            contenuto : "6 Pacchetti di crackers alla farina di barbabietola",
-            immagine : "public/images/cracker_barbabietola.jpeg",
-            tags : ["healty","panetteria","barbabietola"]
-        },
-        {titolo: "Pane dolce fritto",
-            contenuto : "Pane fresco fritto in olio di girasole e zuccherato",
-            immagine : "public/images/pane_fritto_dolce.jpeg",
-            tags : ["pane","panetteria","pasticceria"]
-        },
-        {titolo : "Pasta alla barbabietola",
-            contenuto : "Pasta trafilata al bronzo con farina di barbabietola",
-            immagine : "public/images/pasta_barbabietola.jpeg",
-            tags : ["healty", "pasta","barbabietola"]
-        },
-        {titolo : "Torta paesana",
-            contenuto : "Tipica torta fatta in casa",
-            immagine : "public/images/torta_paesana.jpeg",
-            tags : ["pasticceria", "torta", "goloso"]
-        }
-    ]
-        res.json(products)
+
+
+
+    //  const products = [
+    //     {titolo: "Ciambellone alla crema",
+    //         contenuto : "Goloso ciambellone farcito di crema fatta in casa",
+    //         immagine : "public/images/ciambellone.jpeg",
+    //         tags : ["pasticceria","crema","goloso"]
+    //     },
+    //     {titolo: "Cracker alla Barbabietola",
+    //         contenuto : "6 Pacchetti di crackers alla farina di barbabietola",
+    //         immagine : "public/images/cracker_barbabietola.jpeg",
+    //         tags : ["healty","panetteria","barbabietola"]
+    //     },
+    //     {titolo: "Pane dolce fritto",
+    //         contenuto : "Pane fresco fritto in olio di girasole e zuccherato",
+    //         immagine : "public/images/pane_fritto_dolce.jpeg",
+    //         tags : ["pane","panetteria","pasticceria"]
+    //     },
+    //     {titolo : "Pasta alla barbabietola",
+    //         contenuto : "Pasta trafilata al bronzo con farina di barbabietola",
+    //         immagine : "public/images/pasta_barbabietola.jpeg",
+    //         tags : ["healty", "pasta","barbabietola"]
+    //     },
+    //     {titolo : "Torta paesana",
+    //         contenuto : "Tipica torta fatta in casa",
+    //         immagine : "public/images/torta_paesana.jpeg",
+    //         tags : ["pasticceria", "torta", "goloso"]
+    //     }
+    // ]
+         res.json(prodotti)
 })
 
 
---
+++ prodotti.js
@@ -0,0 +1,29 @@
+const products = [
+    {titolo: "Ciambellone alla crema",
+        contenuto : "Goloso ciambellone farcito di crema fatta in casa",
+        immagine : "public/images/ciambellone.jpeg",
+        tags : ["pasticceria","crema","goloso"]
+    },
+    {titolo: "Cracker alla Barbabietola",
+        contenuto : "6 Pacchetti di crackers alla farina di barbabietola",
+        immagine : "public/images/cracker_barbabietola.jpeg",
+        tags : ["healty","panetteria","barbabietola"]
+    },
+    {titolo: "Pane dolce fritto",
+        contenuto : "Pane fresco fritto in olio di girasole e zuccherato",
+        immagine : "public/images/pane_fritto_dolce.jpeg",
+        tags : ["pane","panetteria","pasticceria"]
+    },
+    {titolo : "Pasta alla barbabietola",
+        contenuto : "Pasta trafilata al bronzo con farina di barbabietola",
+        immagine : "public/images/pasta_barbabietola.jpeg",
+        tags : ["healty", "pasta","barbabietola"]
+    },
+    {titolo : "Torta paesana",
+        contenuto : "Tipica torta fatta in casa",
+        immagine : "public/images/torta_paesana.jpeg",
+        tags : ["pasticceria", "torta", "goloso"]
+    }
+]
+
+module.exports = {products}
